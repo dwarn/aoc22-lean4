cc0c1e0743c93c70de4148fed601927be178b188
David Wärn <codwarn@gmail.com>
2022-12-04 11:16:37 +0100

    day 4

---
+++ 4/a.lean
@@ -0,0 +1,12 @@
+import Std
+
+def f (l : List Char) : Option Nat :=
+match (l.splitOnP (fun c => c = ',' ∨ c = '-')).map (String.toNat! ∘ String.mk) with
+| [a, b, c, d] => some <| if (a <= c && d <= b) || (c <= a && b <= d) then 1 else 0
+| _ => none
+
+def main' : IO (Option Nat) := do
+  let inp ← IO.FS.lines "4/a.in"
+  return Array.foldr ((· + ·) <$> · <*> ·) (some 0) (inp.map (f ∘ String.toList))
+
+--#eval main'
